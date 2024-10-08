bb313cd230e9bc7c55639c0150e7016384957c23
cato-j <cato.jordalen@selstad.no>
2024-10-08 09:45:50 +0200

    4.0.0

--- AutoUpdateTest/Form1.cs
+++ AutoUpdateTest/Form1.cs
@@ -4,21 +4,28 @@ namespace AutoUpdateTest
 {
     public partial class Form1 : Form
     {
-        string pat = "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";
+        internal static string pat = "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";
+        private static string token => $"Bearer {pat}";
+        private static readonly HttpClient client = new HttpClient();
 
         public Form1()
         {
             InitializeComponent();
 
-            label1.Text = "3.0.0";
+            label1.Text = "4.0.0";
 
             CheckForUpdates();
         }
-
-        public void CheckForUpdates()
+        public async Task CheckForUpdates()
         {
-            AutoUpdater.Start("https://github.com/cato-j/AutoUpdateTest/blob/master/latestVersion.xml");
-            AutoUpdater.HttpUserAgent = pat;
+            var updater = new GitHubUpdater();
+            var latest = await updater.GetLatestRelease();
+
+            label1.Text = $"{label1.Text} | {latest.TagName}";
+
+            //AutoUpdater.Start(latest);
+            //AutoUpdater.HttpUserAgent = pat;
+            //AutoUpdater.CurrentVersion = latestVersion;
         }
     }
 }
---
+++ AutoUpdateTest/GitHubUpdater.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoUpdateTest
+{
+    internal class GitHubUpdater
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static string baseLatestUrl = "https://api.github.com/repos/your_username/your_repo/releases/latest";
+        private static string latestUrl
+        {
+            get
+            {
+                string username = "cato-j";
+                string repo = "AutoUpdateTest";
+
+                string s = baseLatestUrl.Replace("your_username", username);
+                s = s.Replace("your_repo", repo);
+                return s;
+            }
+        }
+        private static string token => $"Bearer {Form1.pat}";
+
+        public async Task<GitHubRelease?> GetLatestRelease()
+        {
+            client.DefaultRequestHeaders.Add("User-Agent", "request");
+            client.DefaultRequestHeaders.Add("Authorization", token);
+
+            try
+            {
+                var response = await client.GetStringAsync(latestUrl);
+                GitHubRelease? release = JsonConvert.DeserializeObject<GitHubRelease>(response);
+
+                return release;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public class GitHubRelease
+        {
+            [JsonProperty("tag_name")]
+            public string TagName { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("body")]
+            public string Body { get; set; }
+
+            [JsonProperty("assets")]
+            public List<Asset> Assets { get; set; }
+        }
+
+        public class Asset
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("browser_download_url")]
+            public string BrowserDownloadUrl { get; set; }
+        }
+    }
+}
