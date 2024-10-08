665b3a792c9acebfd16651b52b71d1cf1bb8e577
cato-j <cato.jordalen@selstad.no>
2024-10-08 08:55:44 +0200

    v1

--- AutoUpdateTest/Form1.cs
+++ AutoUpdateTest/Form1.cs
@@ -1,10 +1,24 @@
+using AutoUpdaterDotNET;
+
 namespace AutoUpdateTest
 {
     public partial class Form1 : Form
     {
+        string pat = "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";
+
         public Form1()
         {
             InitializeComponent();
+
+            label1.Text = "1.0.0";
+
+            CheckForUpdates();
+        }
+
+        public void CheckForUpdates()
+        {
+            AutoUpdater.Start("https://your_server_path/update.xml");
+            AutoUpdater.HttpUserAgent = pat;
         }
     }
 }
